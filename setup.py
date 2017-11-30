1e99d03957787fa515869d4351eb54c4183df73b
Benjamin Glick <glick@lclark.edu>
2017-11-29 16:28:19 -0800

    change setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from distutils.core import setup
 
 setup(
-    name='parsl_dav_vis',
+    name='parsl_dag_vis',
     packages=['dag_vis'],
     version='0.1',  # Ideally should be same as your GitHub release tag varsion
     description='DAG visualization code to be run from Jupyter notebooks to visualize data flow dependency graphs of workflows',
     author='Ben Glick',
     author_email='glick@lclark.edu',
+    license='Apache 2.0',
     url='https://github.com/benhg/parsl-dag-vis',
     download_url='https://github.com/benhg/parsl-dag-vis/archive/v0.1-alpha.zip',
-    keywords=['parsl', 'jupyter', 'visualization'],
+    keywords=['parsl', 'jupyter', 'visualization',
+              'Workflows', 'Scientific computing'],
     install_requires=[
         'parsl',
         'jupyter',
         'ipython'
     ],
     classifiers=['Development Status :: 3 - Alpha',
-                 'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python :: 3.6',
-                 'Topic :: Workflow Languages :: Visualization'],
+                 'License :: OSI Approved :: Apache Software License',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6', ],
 )
